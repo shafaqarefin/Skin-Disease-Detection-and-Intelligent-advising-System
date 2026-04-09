e94e401bf157a4e29d4660c659ba45c0c4310b2d
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2026-04-10 01:04:14 +0600

    removed comments.minor changes

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,19 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# --- THE DYNAMIC PATH MAGIC ---
-# 1. Path(__file__).resolve() gets the exact location of this config.py file
-# 2. .parent.parent.parent goes up 3 folders: core -> app -> backend
-# 3. We then attach the models folder and the file name to that root path
+
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-DYNAMIC_MODEL_PATH = str(BASE_DIR / "model" / "my_model.h5")
+DYNAMIC_MODEL_PATH = str(BASE_DIR / "model" / "model_v1.h5")
 
 
 class Settings:
     GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY")
-    # Use our dynamically generated path
     MODEL_PATH: str = DYNAMIC_MODEL_PATH
-    # Updated to the latest Gemini Pro model
     LLM_MODEL: str = "models/gemini-2.5-flash"
 
 
